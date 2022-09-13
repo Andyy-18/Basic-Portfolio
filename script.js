16132bb866a840d13955f51068446dcfecabcbd0
Aninda Banerjee <baninda572@gmail.com>
2022-09-13 22:01:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector('.cross').style.display='none'
+document.querySelector('.hamburger').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+    if(document.querySelector('.sidebar').classList.contains('sidebargo')){
+        document.querySelector('.ham').style.display='inline'
+        document.querySelector('.cross').style.display='none'
+    }
+    else{
+        document.querySelector('.ham').style.display='none'
+        setTimeout(() => {
+            document.querySelector('.cross').style.display='inline'
+        }, 550);
+    }
+})
